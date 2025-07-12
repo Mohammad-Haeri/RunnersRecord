4727c3fd0d14538bdacd474bff562678161ca3ed
Mohammad-Haeri <Haeri.M1989@Gmail.com>
2025-07-12 03:50:44 +0330

    Initial commit

---
+++ src/main/java/ir/Mohammad_Haeri/RunnersRecord/RunnersRecordApplication.java
@@ -0,0 +1,13 @@
+package ir.Mohammad_Haeri.RunnersRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnersRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnersRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/Mohammad_Haeri/RunnersRecord/RunnersRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.Mohammad_Haeri.RunnersRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnersRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
