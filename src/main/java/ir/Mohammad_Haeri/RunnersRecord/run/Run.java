4fb7cef529170340b1a047b72923b02baed59249
Mohammad-Haeri <Haeri.M1989@Gmail.com>
2025-07-20 03:49:11 +0330

    Run Record modified to meet H2 db requirements

--- src/main/java/ir/Mohammad_Haeri/RunnersRecord/run/Run.java
+++ src/main/java/ir/Mohammad_Haeri/RunnersRecord/run/Run.java
@@ -2,13 +2,16 @@ package ir.Mohammad_Haeri.RunnersRecord.run;
 
 import jakarta.validation.constraints.*;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 public record Run(
         @Positive
         int id,
         @NotEmpty
         String title,
-        long startedOn,
-        long completedOn,
+        LocalDateTime startedOn,
+        LocalDateTime completedOn,
         @PositiveOrZero
         int meters,
         @Max(100)
@@ -17,7 +20,7 @@ public record Run(
         Location location
 ) {
         public Run{
-                if (completedOn <= startedOn){
+                if (completedOn.toEpochSecond(ZoneOffset.UTC) <= startedOn.toEpochSecond(ZoneOffset.UTC)){
                         throw new IllegalArgumentException("Starting time cannot be equal or exceed Completion time");
                 }
         }
