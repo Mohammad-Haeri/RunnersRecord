cea020dfd58f9379269b7be9d2dc3e04ae9d1b4b
Mohammad-Haeri <Haeri.M1989@Gmail.com>
2025-07-20 03:50:35 +0330

    Logger add to application file.

--- src/main/java/ir/Mohammad_Haeri/RunnersRecord/RunnersRecordApplication.java
+++ src/main/java/ir/Mohammad_Haeri/RunnersRecord/RunnersRecordApplication.java
@@ -1,13 +1,17 @@
 package ir.Mohammad_Haeri.RunnersRecord;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RunnersRecordApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RunnersRecordApplication.class, args);
-	}
+    private static  final Logger log = LoggerFactory.getLogger(RunnersRecordApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunnersRecordApplication.class, args);
+    }
 
 }
