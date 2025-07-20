247ebbf19da94911080a5bd792a84e18b419d557
Mohammad-Haeri <Haeri.M1989@Gmail.com>
2025-07-20 03:49:55 +0330

    Some seed data added as test purpose.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Run(id, title, started_on, completed_on, meters, score, location)
+VALUES (1,'First Run','2025-06-19 16:47:00','2025-06-19 16:49:00',4010,98,'OUTDOOR');
+INSERT INTO Run(id, title, started_on, completed_on, meters, score, location)
+VALUES (2,'Second Run','2025-06-20 02:48:00','2025-06-20 02:51:00',6212,85,'OUTDOOR');
+INSERT INTO Run(id, title, started_on, completed_on, meters, score, location)
+VALUES (3,'Third Run','2025-06-20 23:47:00','2025-06-20 23:49:00',7650,100,'OUTDOOR');
