5f41ed82c05a3306b529da5e7ea9a3f87d9b4798
Mohammad-Haeri <Haeri.M1989@Gmail.com>
2025-07-23 03:27:01 +0330

    Utils class added. ignore method added to Utils class show result ignorance like python underscore(_) in clean and more verbose way.

---
+++ src/main/java/ir/Mohammad_Haeri/RunnersRecord/Utils.java
@@ -0,0 +1,16 @@
+package ir.Mohammad_Haeri.RunnersRecord;
+
+
+public class Utils {
+    /**
+     * <p>Just to show that the result going to be ignored in the cleanest way</p>
+     *
+     * @param ignored none void method or function as input argument.<br/><br/>
+     * <a href="https://www.Mohammad-Haeri.ir">Portfolio</a> -
+     * <a href="https://github.com/Mohammad-Haeri/">GitHub</a> -
+     * <a href="https://www.linkedin.com/in/Mohammad-Haeri/">LinkedIn</a>
+     * @author Mohammad-Haeri.
+     */
+    public static void ignore(Object... ignored) {
+    }
+}
