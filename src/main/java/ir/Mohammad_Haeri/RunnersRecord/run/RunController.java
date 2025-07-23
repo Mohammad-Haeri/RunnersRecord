3b830224201f7ea046035424c7dcbb8198ae0332
Mohammad-Haeri <Haeri.M1989@Gmail.com>
2025-07-23 05:35:41 +0330

    IRunRecordDataJdbcCrudRepository methods added to RunController.

--- src/main/java/ir/Mohammad_Haeri/RunnersRecord/run/RunController.java
+++ src/main/java/ir/Mohammad_Haeri/RunnersRecord/run/RunController.java
@@ -11,9 +11,11 @@ import java.util.List;
 public class RunController {
 
     private final RunnersRecordRepository repository;
+    private final IRunRecordDataJdbcCrudRepository dataJdbcCrudRepository;
 
-    public RunController(RunnersRecordRepository repository) {
+    public RunController(RunnersRecordRepository repository, IRunRecordDataJdbcCrudRepository dataJdbcCrudRepository) {
         this.repository = repository;
+        this.dataJdbcCrudRepository = dataJdbcCrudRepository;
     }
 
     @GetMapping("/test")
@@ -51,6 +53,38 @@ public class RunController {
     public void delete(@PathVariable int id){
         this.repository.delete(id);
     }
+
+    @GetMapping("/byLocation/{location}")
+    public List<Run> getAllByLocation(@PathVariable String location){
+        if (location.equals(Location.OUTDOOR.name())){
+            return this.dataJdbcCrudRepository.findAllByLocation(Location.OUTDOOR);
+        } else if (location.equals(Location.INDOOR.name())) {
+            return this.dataJdbcCrudRepository.findAllByLocation(Location.INDOOR);
+        }
+
+        return null;
+
+    }
+
+    @GetMapping("/greaterThan/{score}")
+    public List<Run> getAllByScoreGreaterThan(@PathVariable int score){
+        return this.dataJdbcCrudRepository.findAllByScoreGreaterThan(score);
+
+    }
+
+    @GetMapping("/lessThan/{score}")
+    public List<Run> getAllByScoreIsLessThan(@PathVariable int score){
+        return this.dataJdbcCrudRepository.findAllByScoreIsLessThan(score);
+
+    }
+
+    @GetMapping("/between/{min}/{max}")
+    public List<Run> getAllByScoreBetween(@PathVariable int min, @PathVariable int max){
+        return this.dataJdbcCrudRepository.findAllByScoreBetween(min, max);
+
+    }
+
+
 
 
 }
