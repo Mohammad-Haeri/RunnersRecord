5d0b4c6cd179f792456171c845ff0eef93d9eeff
Mohammad-Haeri <Haeri.M1989@Gmail.com>
2025-07-27 16:20:18 +0330

    Unit test for controller added.

---
+++ src/test/java/ir/Mohammad_Haeri/RunnersRecord/run/RunControllerTest.java
@@ -0,0 +1,127 @@
+package ir.Mohammad_Haeri.RunnersRecord.run;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+@Import({RunnersRecordRepository.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class RunControllerTest {
+
+    @Autowired
+    RunnersRecordRepository repository;
+
+    @Autowired
+    IRunRecordDataJdbcCrudRepository jdbcRepository;
+
+    private boolean brittleness = true;
+    private final int testId = 10000;
+
+    @Test
+    @Order(1)
+    void getAll() {
+        var all = this.repository.getAll();
+        assertEquals(3, all.size());
+    }
+
+    @Test
+    @Order(2)
+    void getById() {
+        Optional<Run> run = this.repository.getById(1);
+        assertTrue(run.isPresent());
+    }
+
+    @Test
+    @Order(3)
+    void create() {
+        var allBeforeInsert = this.repository.getAll();
+        Run newRun = new Run(
+                testId,
+                "Test Run",
+                LocalDateTime.now().minusMinutes(4),
+                LocalDateTime.now(),
+                5428,
+                90,
+                Location.INDOOR);
+        this.repository.create(newRun, false);
+        var allAfterInsert = this.repository.getAll();
+
+        assertTrue(allBeforeInsert.size() < allAfterInsert.size());
+        brittleness = false;
+    }
+
+    @Test
+    @Order(4)
+    void update() {
+        assumeTrue(brittleness,
+                "Skipping update due to create() step failed or was skipped");
+
+        Run newRun = new Run(
+                testId,
+                "Fourth Run Updated",
+                LocalDateTime.now().minusMinutes(4),
+                LocalDateTime.now(),
+                6000,
+                90,
+                Location.OUTDOOR);
+        this.repository.update(newRun, testId);
+
+        var updatedRun = repository.getById(testId);
+
+        if (updatedRun.isEmpty()) {
+            return;
+        }
+        assertEquals(6000, updatedRun.get().meters());
+
+    }
+
+    @Test
+    @Order(5)
+    void delete() {
+
+        this.repository.delete(testId);
+        var deletedRun = repository.getById(testId);
+
+        assertTrue(deletedRun.isEmpty());
+
+    }
+
+    @Test
+    void getAllByLocation() {
+        var allByLocation = jdbcRepository.findAllByLocation(Location.OUTDOOR);
+        assertFalse(allByLocation.isEmpty());
+    }
+
+    @Test
+    void getAllByScoreGreaterThan() {
+        var allByScoreGreaterThan = jdbcRepository.findAllByScoreGreaterThan(90);
+        assertFalse(allByScoreGreaterThan.isEmpty());
+    }
+
+    @Test
+    void getAllByScoreIsLessThan() {
+        var allByScoreIsLessThan = jdbcRepository.findAllByScoreIsLessThan(95);
+        assertFalse(allByScoreIsLessThan.isEmpty());
+    }
+
+    @Test
+    void getAllByScoreBetween() {
+        var allByScoreBetween = jdbcRepository.findAllByScoreBetween(90, 100);
+        assertFalse(allByScoreBetween.isEmpty());
+    }
+}
